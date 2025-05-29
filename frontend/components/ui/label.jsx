25ff64675d1a7368cf72b2c48a8ee84ff7316b98
Aniket <aniketsoni3529@gmail.com>
2025-05-29 14:42:38 +0530

    Lastest Commit

--- frontend/components/ui/label.jsx
+++ frontend/components/ui/label.jsx
@@ -1,9 +1,27 @@
+// 'use client';
+
+// import * as React from 'react';
+// import { cn } from '@/lib/utils';
+
+// export const Label = React.forwardRef(({ className, ...props }, ref) => (
+//   <label
+//     ref={ref}
+//     className={cn(
+//       'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+//       className
+//     )}
+//     {...props}
+//   />
+// ));
+
+// Label.displayName = 'Label';
+
 'use client';
 
-import * as React from 'react';
+import React, { forwardRef } from 'react';
 import { cn } from '@/lib/utils';
 
-export const Label = React.forwardRef(({ className, ...props }, ref) => (
+const Label = forwardRef(({ className, ...props }, ref) => (
   <label
     ref={ref}
     className={cn(
@@ -16,3 +34,4 @@ export const Label = React.forwardRef(({ className, ...props }, ref) => (
 
 Label.displayName = 'Label';
 
+export default Label;
